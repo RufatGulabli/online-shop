04a46c439a0e3a32e13b4328ba67b2299e925cb8
Rufat Gulabli <gulabli.rufat@gmail.com>
2019-04-23 23:22:50 +0400

    first commit

---
+++ src/app/material-components.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import {
+  MatSelectModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatButtonModule,
+  MatChipsModule,
+  MatProgressSpinnerModule,
+  MatTooltipModule,
+  MatTabsModule,
+  MatDialogModule
+} from "@angular/material";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatDialogModule
+  ]
+})
+export class MaterialComponentsModule {}
